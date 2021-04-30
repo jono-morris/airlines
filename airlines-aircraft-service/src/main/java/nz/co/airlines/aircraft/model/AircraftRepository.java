aba2a90c63ae7ab958e39caf43dd8d7915820789
Jono <jono.morris@xtra.co.nz>
2021-05-01 00:27:56 +1200

    Add Aircraft Repository and Resource  methods.

--- airlines-aircraft-service/src/main/java/nz/co/airlines/aircraft/model/AircraftRepository.java
+++ airlines-aircraft-service/src/main/java/nz/co/airlines/aircraft/model/AircraftRepository.java
@@ -1,7 +1,17 @@
 package nz.co.airlines.aircraft.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AircraftRepository extends JpaRepository<Aircraft, Integer>  {
 
+    
+    @Query("from Aircraft aircraft " +
+           "left join fetch aircraft.seats " +
+            "where aircraft.flightId = :flightId")
+    Aircraft findByFlightIdAndFetchSeats(
+          @Param("flightId") Integer flightId);
+    
+    Aircraft findByFlightId(Integer flightId);
 }
--- airlines-aircraft-service/src/main/java/nz/co/airlines/aircraft/model/web/AircraftResource.java
+++ airlines-aircraft-service/src/main/java/nz/co/airlines/aircraft/model/web/AircraftResource.java
@@ -3,6 +3,7 @@ package nz.co.airlines.aircraft.model.web;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,18 @@ public class AircraftResource {
     @GetMapping(value = "/")
     public List<Aircraft> findAll() {
         return aircraftRepository.findAll();
+    }
+    
+    @GetMapping(value = "/flight/{flightId}")
+    public Aircraft findByFlightIdAndFetchSeats(
+            @PathVariable("flightId") Integer flightId) {
+        
+        return aircraftRepository.findByFlightIdAndFetchSeats(flightId);
+    }
+    
+    @GetMapping(value = "/seats/flight/{flightId}")
+    public Integer findByFlightId(
+            @PathVariable("flightId") Integer flightId) {
+        return aircraftRepository.findByFlightId(flightId).freeSeats();
     }
 }
